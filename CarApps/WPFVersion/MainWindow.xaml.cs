97a5bf9571229a3d1038324bb605a98707aefd9f
WolfManR <wolfmanr@mail.ru>
2019-11-25 18:46:13 +0300

    Add Click realisation for Add Car and Change Color buttons

--- CarApps/WPFVersion/MainWindow.xaml.cs
+++ CarApps/WPFVersion/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using WPFVersion.Models;
 
@@ -16,6 +17,17 @@ namespace WPFVersion
             _cars.Add(new Inventory { CarId = 1, Color = "Blue", Make = "Chevy", PetName = "Kit" });
             _cars.Add(new Inventory { CarId = 2, Color = "Red", Make = "Ford", PetName = "Red Rider" });
             cboCars.ItemsSource = _cars;
+        }
+
+        private void btnChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            _cars.First(x => x.CarId == ((Inventory)cboCars.SelectedItem)?.CarId).Color = "Pink";
+        }
+
+        private void btnAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            var maxCount = _cars?.Max(x => x.CarId) ?? 0;
+            _cars?.Add(new Inventory { CarId = ++maxCount, Color = "Yellow", Make = "VM", PetName = "Birdie" });
         }
     }
 }
