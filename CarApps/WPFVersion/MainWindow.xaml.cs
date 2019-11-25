c58952055c7224340397e042d151d8ac7879c348
WolfManR <wolfmanr@mail.ru>
2019-11-25 17:38:59 +0300

    Add Bindings and IList<inventory> in Code Behind and fill it

--- CarApps/WPFVersion/MainWindow.xaml.cs
+++ CarApps/WPFVersion/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WPFVersion.Models;
 
 namespace WPFVersion
 {
@@ -20,9 +9,13 @@ namespace WPFVersion
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly IList<Inventory> _cars = new List<Inventory>();
         public MainWindow()
         {
             InitializeComponent();
+            _cars.Add(new Inventory { CarId = 1, Color = "Blue", Make = "Chevy", PetName = "Kit" });
+            _cars.Add(new Inventory { CarId = 2, Color = "Red", Make = "Ford", PetName = "Red Rider" });
+            cboCars.ItemsSource = _cars;
         }
     }
 }
