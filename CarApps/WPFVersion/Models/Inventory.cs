7357f6796dc47e07b0942b5f552d671ee560a718
WolfManR <wolfmanr@mail.ru>
2019-11-25 18:57:49 +0300

    Add INotifyPropertyChanged realisation in Inventory Class

--- CarApps/WPFVersion/Models/Inventory.cs
+++ CarApps/WPFVersion/Models/Inventory.cs
@@ -1,10 +1,61 @@
-﻿namespace WPFVersion.Models
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFVersion.Models
 {
-    public class Inventory
+    public class Inventory : INotifyPropertyChanged
     {
-        public int CarId { get; set; }
-        public string Make { get; set; }
-        public string Color { get; set; }
-        public string PetName { get; set; }
+        private int carId;
+        private string color;
+        private string make;
+        private string petName;
+
+        public int CarId
+        {
+            get => carId;
+            set
+            {
+                if (carId == value) return;
+                carId = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Make
+        {
+            get => make;
+            set
+            {
+                if (make == value) return;
+                make = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Color
+        {
+            get => color;
+            set
+            {
+                if (color == value) return;
+                color = value;
+                OnPropertyChanged(nameof(Color));  //Attribute CallerMemberName not needed with "nameof()" method
+            }
+        }
+        public string PetName
+        {
+            get => petName; 
+            set
+            {
+                if (petName == value) return;
+                petName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
