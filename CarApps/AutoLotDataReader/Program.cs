1c0e0feb541211b2a88f88e1a8d36e8e5bf4de18
Ivan <wolfmanr@mail.ru>
2019-11-26 18:39:18 +0300

    add Code Connection to DataBase to AutoLotDataReader Project to read data from it with DataReader

--- CarApps/AutoLotDataReader/Program.cs
+++ CarApps/AutoLotDataReader/Program.cs
@@ -35,6 +35,60 @@ namespace AutoLotDataReader
 
         static void Main(string[] args)
         {
+            WriteLine("***** Fun with Data Readers *****\n");
+
+            // Create a connection string via the builder object.
+            var cnStringBuilder = new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "AutoLot",
+                DataSource = @"(localdb)\mssqllocaldb",
+                ConnectTimeout = 30,
+                IntegratedSecurity = true
+            };
+
+            // Create an open a connection.
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = cnStringBuilder.ConnectionString;
+                connection.Open();
+
+                ShowConnectionStatus(connection);
+
+                // Create a SQL command object.
+                string sql = "Select * From Inventory;Select * from Customers";
+                using (SqlCommand myCommand = new SqlCommand(sql, connection))
+                {
+                    //iterate over the inventory & customers
+                    // Obtain a data reader a la ExecuteReader().
+                    using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                    {
+                        do
+                        {
+                            while (myDataReader.Read())
+                            {
+                                WriteLine("***** Record *****");
+                                for (int i = 0; i < myDataReader.FieldCount; i++)
+                                {
+                                    WriteLine($"{myDataReader.GetName(i)} = {myDataReader.GetValue(i)}");
+                                }
+                                WriteLine();
+                            }
+                        } while (myDataReader.NextResult());
+                    }
+                }
+            }
+
+            ReadLine();
+        }
+
+        static void ShowConnectionStatus(SqlConnection connection)
+        {
+            // Show various stats about current connection object.
+            WriteLine("***** Info about your connection *****");
+            WriteLine($"Database location: {connection.DataSource}");
+            WriteLine($"Database name: {connection.Database}");
+            WriteLine($"Timeout: {connection.ConnectionTimeout}");
+            WriteLine($"Connection state: {connection.State}\n");
         }
     }
 }
