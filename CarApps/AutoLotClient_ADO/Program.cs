d68e9b13dfde9e30069b8351f3f2cc42c9e8433a
Ivan <wolfmanr@mail.ru>
2019-11-26 22:39:24 +0300

    add connection to AutoLotClient_ADO to BulkImport

--- CarApps/AutoLotClient_ADO/Program.cs
+++ CarApps/AutoLotClient_ADO/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
+using AutoLotDAL_ADO.BulkImport;
 using AutoLotDAL_ADO.DataOperations;
 using AutoLotDAL_ADO.Models;
 
