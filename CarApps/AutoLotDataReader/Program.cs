3802ae51c7169750820d8495ea7b28d6197f0fdc
Ivan <wolfmanr@mail.ru>
2019-11-26 18:01:13 +0300

    add AutoLotDataReader Project

---
+++ CarApps/AutoLotDataReader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SqlClient;
+
+using static System.Console;
+
+namespace AutoLotDataReader
+{
+    class Program
+    {
+        static void FirstVersion()
+        {
+            WriteLine("***** Fun with Data Readers *****\n");
+            // Create and open a connection.
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = @"Data Source=(localdb)\mssqllocaldb;Integrated Security=true;" +
+                                               "Initial Catalog=AutoLot";
+                connection.Open();
+
+                // Create a SQL command object.
+                string sql = "Select * From Inventory";
+                SqlCommand myCommand = new SqlCommand(sql, connection);
+
+                // Obtain a data reader a la ExecuteReader().
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    // Loop over the results.
+                    while (myDataReader.Read()) WriteLine($"-> Make: {myDataReader["Make"]}, " +
+                                                             $"PetName: {myDataReader["PetName"]}, " +
+                                                             $"Color: {myDataReader["Color"]}.");
+                }
+            }
+            ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
