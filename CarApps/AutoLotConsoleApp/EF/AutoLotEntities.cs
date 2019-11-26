51b99ceeeec02874fed87722f8c45ae36c677077
Ivan <wolfmanr@mail.ru>
2019-11-26 23:29:40 +0300

    Add realisation of EF Code First template

---
+++ CarApps/AutoLotConsoleApp/EF/AutoLotEntities.cs
@@ -0,0 +1,28 @@
+namespace AutoLotConsoleApp.EF
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AutoLotEntities : DbContext
+    {
+        public AutoLotEntities()
+            : base("name=AutoLotConnection")
+        {
+        }
+
+        public virtual DbSet<CreditRisk> CreditRisk { get; set; }
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<Inventory> Inventory { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inventory>()
+                .HasMany(e => e.Orders)
+                .WithRequired(e => e.Inventory)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
