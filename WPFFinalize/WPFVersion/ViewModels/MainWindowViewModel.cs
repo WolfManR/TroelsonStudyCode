5685b7af90414dc242eb3eeb7d6413ef9e1eea7e
Ivan <wolfmanr@mail.ru>
2019-11-27 23:19:51 +0300

    add realisation of update DB if data in app changes

--- WPFFinalize/WPFVersion/ViewModels/MainWindowViewModel.cs
+++ WPFFinalize/WPFVersion/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using AutoLotDAL_EF.Models;
 using AutoLotDAL_EF.Repos;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Windows.Input;
 using WPFVersion.Cmds;
 
@@ -13,6 +15,7 @@ namespace WPFVersion.ViewModels
         private ICommand _addCarCommand = null;
         private RelayCommand<Inventory> _deleteCarCommand = null;
 
+        InventoryRepo inventory = new InventoryRepo();
         public IList<Inventory> Cars { get; }
         public ICommand ChangeColorCmd => _changeColorCommand ?? (_changeColorCommand = new ChangeColorCommand());
         public ICommand AddCarCmd => _addCarCommand ?? (_addCarCommand = new AddCarCommand());
@@ -20,8 +23,39 @@ namespace WPFVersion.ViewModels
 
         public MainWindowViewModel()
         {
-            Cars = new ObservableCollection<Inventory>(new InventoryRepo().GetAll());
-        }        
+            var inventories = new ObservableCollection<Inventory>(inventory.GetAll());
+            inventories.CollectionChanged += Cars_CollectionChanged;
+            Cars = inventories;
+            
+        }
+
+        private void Cars_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var action = e.Action;
+            switch (action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (Inventory item in e.NewItems)
+                    {
+                        inventory.Add(item);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (Inventory item in e.OldItems)
+                    {
+                        inventory.Delete(item);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    break;
+                default:
+                    break;
+            }
+        }
 
         private bool CanDeleteCar(Inventory car) => car != null;
         private void DeleteCar(Inventory car)
