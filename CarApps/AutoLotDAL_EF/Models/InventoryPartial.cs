94052ebe532c68bf3b25b67b26603eaa2ff99084
Ivan <wolfmanr@mail.ru>
2019-11-27 13:55:09 +0300

    fix error and change migration final to success update

--- CarApps/AutoLotDAL_EF/Models/InventoryPartial.cs
+++ CarApps/AutoLotDAL_EF/Models/InventoryPartial.cs
@@ -2,14 +2,14 @@
 
 namespace AutoLotDAL_EF.Models
 {
-    public partial class Inventory
+    public partial class Inventory:EntityBase
     {
         [NotMapped]
         public string MakeColor => $"{Make} + ({Color})";
         public override string ToString()
         {
             // Since the PetName column could be empty, supply the default name of **No Name**.
-            return $"{this.PetName ?? "**No Name**"} is a {this.Color} {this.Make} with ID {this.CarId}.";
+            return $"{this.PetName ?? "**No Name**"} is a {this.Color} {this.Make} with ID {this.Id}.";
         }
     }
 }
