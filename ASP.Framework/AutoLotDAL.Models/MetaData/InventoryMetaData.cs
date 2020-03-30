4aa8126211879ed83f860d27003a45e846cba4b6
Tarxos <wolfmanr@mail.ru>
2020-03-31 00:19:08 +0300

    add final edit to code for MVC Project

--- ASP.Framework/AutoLotDAL.Models/MetaData/InventoryMetaData.cs
+++ ASP.Framework/AutoLotDAL.Models/MetaData/InventoryMetaData.cs
@@ -6,5 +6,7 @@ namespace AutoLotDAL.Models.MetaData
     {
         [Display(Name ="Pet Name")]
         public string PetName;
+        [StringLength(50,ErrorMessage ="Please enter a value less than 50 characters long")]
+        public string Make;
     }
 }
