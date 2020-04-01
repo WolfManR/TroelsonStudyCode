2c021a6b1f1195fb2a059dfdc27e24facdefebe2
Tarxos <wolfmanr@mail.ru>
2020-04-01 14:25:12 +0300

    add last CRUD commands

--- ASPWebApi/ASPWebApi/CarLotWebAPI/Controllers/InventoryController.cs
+++ ASPWebApi/ASPWebApi/CarLotWebAPI/Controllers/InventoryController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -45,6 +46,72 @@ namespace CarLotWebAPI.Controllers
                 return NotFound();
             }
             return Ok(Mapper.Map<Inventory, Inventory>(inventory));
+        }
+
+        [HttpPut,Route("{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutInventory(int id,Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != inventory.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _repo.Save(inventory);
+            }
+            catch (Exception ex)
+            {
+                // В производственном приложении здесь должны быть дополнительные действия
+                throw;
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Inventory
+        [HttpPost,Route("")]
+        [ResponseType(typeof(Inventory))]
+        public IHttpActionResult PostInventory(Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                _repo.Add(inventory);
+            }
+            catch (Exception ex)
+            {
+                // В производственном приложении здесь должны быть дополнительные действия
+                throw;
+            }
+            return CreatedAtRoute("DisplayRoute", new { id = inventory.Id }, inventory);
+        }
+
+        // DELETE: api/Inventory/5
+        [HttpDelete,Route("{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult DeleteInventory(int id,Inventory inventory)
+        {
+            if (id != inventory.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _repo.Delete(inventory);
+            }
+            catch (Exception ex)
+            {
+                // В производственном приложении здесь должны быть дополнительные действия
+                throw;
+            }
+            return Ok();
         }
 
         protected override void Dispose(bool disposing)
