2d57812cbf4411d6d52c0da4bbb44edda77257d9
Tarxos <wolfmanr@mail.ru>
2020-04-02 13:26:03 +0300

    add Models for EFData

---
+++ NetCore/CoreAuto/EFLibraries/AutoLotDAL_Core2.Models/CreditRisk.cs
@@ -0,0 +1,13 @@
+﻿using AutoLotDAL_Core2.Models.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoLotDAL_Core2.Models
+{
+    public class CreditRisk : EntityBase
+    {
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+    }
+}
