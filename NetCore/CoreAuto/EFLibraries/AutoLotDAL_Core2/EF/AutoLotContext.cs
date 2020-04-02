73a47364bc1b39b755daddda04c1721205e719b7
Tarxos <wolfmanr@mail.ru>
2020-04-02 17:31:06 +0300

    add AutoLotContext class

---
+++ NetCore/CoreAuto/EFLibraries/AutoLotDAL_Core2/EF/AutoLotContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoLotDAL_Core2.EF
+{
+    public class AutoLotContext:DbContext
+    {
+        internal AutoLotContext()
+        {
+
+        }
+        public AutoLotContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"server=(LocalDb)\MSSQLLocalDB;database=AutoLotCore2;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework;",
+                    options => options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
