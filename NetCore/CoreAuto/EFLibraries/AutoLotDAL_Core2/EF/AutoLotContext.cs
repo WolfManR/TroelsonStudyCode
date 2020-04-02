f81645dfc2d68cca9d009c25a88261db64d66417
Tarxos <wolfmanr@mail.ru>
2020-04-02 20:32:01 +0300

    add GetTableName method

--- NetCore/CoreAuto/EFLibraries/AutoLotDAL_Core2/EF/AutoLotContext.cs
+++ NetCore/CoreAuto/EFLibraries/AutoLotDAL_Core2/EF/AutoLotContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoLotDAL_Core2.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using System;
 using System.Collections.Generic;
@@ -24,6 +25,31 @@ namespace AutoLotDAL_Core2.EF
                 optionsBuilder.UseSqlServer(@"server=(LocalDb)\MSSQLLocalDB;database=AutoLotCore2;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework;",
                     options => options.EnableRetryOnFailure());
             }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Создать индекс, включающий несколько столбцов
+            modelBuilder.Entity<CreditRisk>(entity =>
+            {
+                entity.HasIndex(e => new { e.FirstName, e.LastName }).IsUnique();
+            });
+
+            // Установить параметр каскадирования на отношении
+            modelBuilder.Entity<Order>()
+                .HasOne(e => e.Car)
+                .WithMany(e => e.Orders)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+
+        public DbSet<CreditRisk> CreditRisks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Inventory> Cars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public string GetTableName(Type type)
+        {
+            return Model.FindEntityType(type).GetTableName();
         }
     }
 }
