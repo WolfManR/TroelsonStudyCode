5d1a954b658d1de75203e5bc37c159032d439ff2
Tarxos <wolfmanr@mail.ru>
2020-04-06 20:19:19 +0300

    add base realization custom descriptor

---
+++ NetCore/CoreMVC/AutoLotMVC_Core2/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AutoLotMVC_Core2.TagHelpers
+{
+    public class EmailTagHelper:TagHelper
+    {
+        public string EmailName { get; set; }
+        public string EmailDomain { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";  // Заменяет <email> дескриптором <a>
+            var address = EmailName + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+}
