0b64d6545cf763a83c1b4fc9fdb1960244d74d40
Tarxos <wolfmanr@mail.ru>
2020-04-13 23:33:28 +0300

    add EF Libraries Projects

---
+++ NetCore/RESTCore/EFLibraries/AutoLotDAL_Core2/EF/AutoLotContext.cs
@@ -0,0 +1,55 @@
+﻿using AutoLotDAL_Core2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoLotDAL_Core2.EF
+{
+    public class AutoLotContext:DbContext
+    {
+        internal AutoLotContext()
+        {
+
+        }
+        public AutoLotContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"server=(LocalDb)\MSSQLLocalDB;database=AutoLotCore2;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework;",
+                    options => options.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Создать индекс, включающий несколько столбцов
+            modelBuilder.Entity<CreditRisk>(entity =>
+            {
+                entity.HasIndex(e => new { e.FirstName, e.LastName }).IsUnique();
+            });
+
+            // Установить параметр каскадирования на отношении
+            modelBuilder.Entity<Order>()
+                .HasOne(e => e.Car)
+                .WithMany(e => e.Orders)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+
+        public DbSet<CreditRisk> CreditRisks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Inventory> Cars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public string GetTableName(Type type)
+        {
+            return Model.FindEntityType(type).GetTableName();
+        }
+    }
+}
---
+++ NetCore/RESTCore/EFLibraries/AutoLotDAL_Core2/EF/AutoLotContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AutoLotDAL_Core2.EF
+{
+    public class AutoLotContextFactory : IDesignTimeDbContextFactory<AutoLotContext>
+    {
+        public AutoLotContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AutoLotContext>();
+            optionsBuilder.UseSqlServer(@"server=(LocalDb)\MSSQLLocalDB;database=AutoLotCore2;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework;",
+                    options => options.EnableRetryOnFailure());
+            return new AutoLotContext(optionsBuilder.Options);
+        }
+    }
+}
